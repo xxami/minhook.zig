b8be92a6ad81e063dee953bcf4c7b6a05dedbb2b
lilah <lilah.willacott@gmail.com>
2021-05-27 18:08:16 +0100

    correct build error

--- build.zig
+++ build.zig
@@ -1,22 +1,14 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const cflags = &[_][]const u8{
-        "-std=c11",
-        "-masm=intel",
-    };
-
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("minhook", "src/minhook.zig");
-
-    lib.addIncludeDir("src/minhook");
-    lib.addIncludeDir("src/minhook/hde");
-    lib.addCSourceFile("src/minhook/buffer.c", cflags);
-    lib.addCSourceFile("src/minhook/hook.c", cflags);
-    lib.addCSourceFile("src/minhook/trampoline.c", cflags);
-    lib.addCSourceFile("src/minhook/hde/hde64.c", cflags);
-    
+    const lib = b.addStaticLibrary("minhook.zig", "src/minhook.zig");
     lib.setBuildMode(mode);
-    lib.linkLibC();
     lib.install();
+
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
 }
